cb27ea3c01c7651ecf099697230035ce7e5cde28
Andis <andislazdins2@gmail.com>
2018-05-03 17:38:33 +0300

    Forth Commit

--- src/main/java/tehinternets/homepage/resource/HomepageController.java
+++ src/main/java/tehinternets/homepage/resource/HomepageController.java
@@ -2,7 +2,9 @@ package tehinternets.homepage.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import tehinternets.homepage.domain.Response;
 import tehinternets.homepage.service.AdvertesmentService;
@@ -35,5 +37,6 @@ public class HomepageController {
         module.put("ad", advertesmentService.getAd());
         return "main";
     }
+
 }
 //object oriented design patterns - gang of four
--- src/main/java/tehinternets/homepage/service/AdvertesmentService.java
+++ src/main/java/tehinternets/homepage/service/AdvertesmentService.java
@@ -4,17 +4,35 @@ import org.springframework.stereotype.Service;
 import tehinternets.homepage.domain.Advertesment;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 @Service
 public class AdvertesmentService {
     public Advertesment getAd() {
-        Advertesment ad = new Advertesment(1L, "pardod valsti", "par 10000000000$", "Janka");
-        return ad;
+        return new Advertesment(1L, "pardod valsti", "par 10000000000$", "Janka");
+
     }
     public ArrayList getAdList() {
         ArrayList<Advertesment> ads = new ArrayList<>();
         Advertesment ad = new Advertesment(1L, "pardod valsti", "par 10000000000$", "Janka");
         ads.add(ad);
+        ads.add(new Advertesment(2L, "pardodu burku", "par 12345125", "Peters"));
+        return ads;
+    }
+
+    public Advertesment searchAdList(ArrayList<Advertesment> ads, String searchQuery) {
+        Advertesment ad = null;
+        for (Advertesment ad1 : ads) {
+            if (ad1.getAuthor().matches(searchQuery)) {
+                ad = ad1;
+            }
+        }
+        
+        return ad;
+    }
+
+    public ArrayList reversAds(ArrayList<Advertesment> ads) {
+        Collections.reverse(ads);
         return ads;
     }
 }
